1502470f1c6f30c0ef88b4a24c1a15514034f903
jesa <wirananta25@gmail.com>
2022-12-06 04:27:16 +0700

    kedua

--- app/Views/alternatif/index.php
+++ app/Views/alternatif/index.php
@@ -17,7 +17,6 @@
         <table class="table table-bordered table-hover table-striped">
             <thead>
                 <tr>
-                    <h1>haha</h1>
                     <th>Kode</th>
                     <th>Nama alternatif</th>
                     <th>NIM</th>
